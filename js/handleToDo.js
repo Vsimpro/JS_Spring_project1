71ef5af633880943b1f7d40fb42f0169010b8f1d
Vsimpro <jsimrpo@gmail.com>
2022-02-22 19:16:22 +0200

    switched from form to spandex

--- js/handleToDo.js
+++ js/handleToDo.js
@@ -1,12 +1,41 @@
-function validateForm(){
-    var task = document.forms["myForm"]["newTask"];
-    //alert("Debug purposes: " + task.value);
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
 
-    if(task.value == "new task") {
-        alert("New task can't be blank!")
+
+function validateForm(){
+    var list = document.createElement("li");
+    var inputVal = document.getElementById("myTask")
+    var user = document.createTextNode(inputVal)
+    
+    list.appendChild(user);
+    
+    if(inputVal === "") {
+        alert("New task can't be blank!")   
+    } else {
+        document.getElementById("tasks").appendChild(list)
     }
-    if (task.value.length <= 3){
-        alert("New task must be over 3 characters long!")
-    }
+    
+    document.getElementById("myTask").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("pppp");
+    span.className = "close";
+    span.appendChild(txt);
+    list.appendChild(span);
+  
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+          var div = this.parentElement;
+          div.style.display = "none";
+        }
+      }
     
 }
