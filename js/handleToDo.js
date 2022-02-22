21ef50dcba263a6c15f93d190ca906ad70010e15
Vsimpro <jsimrpo@gmail.com>
2022-02-22 19:33:43 +0200

    added error 'fyunc.'

--- js/handleToDo.js
+++ js/handleToDo.js
@@ -1,3 +1,4 @@
+
 // Create a "close" button and append it to each list item
 var myNodelist = document.getElementsByTagName("LI");
 var i;
@@ -9,27 +10,39 @@ for (i = 0; i < myNodelist.length; i++) {
   myNodelist[i].appendChild(span);
 }
 
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+function error() {
+    alert("New task can't be blank!")  
+}
 
 function validateForm(){
-    var list = document.createElement("li");
-    var inputVal = document.getElementById("myTask")
-    var user = document.createTextNode(inputVal)
+    var li = document.createElement("li");
+    var inputVal = document.getElementById("myTask").value;
+    var user = document.createTextNode(inputVal);
     
-    list.appendChild(user);
-    
-    if(inputVal === "") {
-        alert("New task can't be blank!")   
+    li.appendChild(user);
+    if(inputVal == "") {
+        error()
     } else {
-        document.getElementById("tasks").appendChild(list)
+        document.getElementById("tasks").appendChild(li)
     }
     
     document.getElementById("myTask").value = "";
 
     var span = document.createElement("SPAN");
-    var txt = document.createTextNode("pppp");
+    var txt = document.createTextNode("X");
     span.className = "close";
     span.appendChild(txt);
-    list.appendChild(span);
+    li.appendChild(span);
   
     for (i = 0; i < close.length; i++) {
         close[i].onclick = function() {
